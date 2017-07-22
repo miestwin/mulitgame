b2753dc59ed0dee810c58251a32677258317943a
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2017-07-22 10:58:56 +0200

    qrcode

--- src/games/index.ts
+++ src/games/index.ts
@@ -20,19 +20,35 @@ function startApp(): void {
     };
 
     const game = new SimpleGame(gameConfig);
-
-    // let qr = new QRious.QRious({
-    //     element: document.getElementById('game'),
-    //     value: 'https://github.com/miestwin'
-    // });
 }
 
 class SimpleGame extends Phaser.Game {
     private socket;
+    private qr;
     constructor (config) {
         super(config);
         this.socket = io();
-        // add states
+        this.state.add('FirstState', FirstState);
+        this.state.start('FirstState');
+    }
+}
+
+class FirstState extends Phaser.State {
+    private qr;
+
+    public preload() {
+        let that = this;
+        this.qr = new QRious({
+            value: 'https://github.com/miestwin'
+        });
+        this. qr = this.qr.toDataURL('image/jpeg');
+        let data = new Image();
+        data.src = this.qr;
+        that.game.cache.addImage('image-data', data.src, data);
+    }
+
+    public create() {
+         this.game.add.sprite(0, 0, 'image-data');
     }
 }
 
